6d742552095cbcde39ef4626295db5ed18d4bf17
leehan <yye572@nate.com>
2017-03-15 11:44:15 +0900

    nested class, interface

--- JavaWork/src/java8/NestedClassTest.java
+++ JavaWork/src/java8/NestedClassTest.java
@@ -1,5 +1,17 @@
 package java8;
 
+interface Showable{
+	void show();
+	interface Message{
+		void msg();
+	}
+}
+class NestedInterface1 implements Showable.Message{
+	public void msg(){
+		System.out.println("Hello nested interface");
+	}
+}
+
 public class NestedClassTest {
 	//중첩 class 종류
 	//1. non-static inner class
@@ -7,10 +19,21 @@ public class NestedClassTest {
 	//3. anonymous class
 	//4. local class
 	public static void main(String[] args) {
+		System.out.println("Nested interface Test!!");
+		Showable.Message message = new NestedInterface1(); //upcasting...
+		message.msg();
+		System.out.println("--------------------------------------");
+		
 		method2();
+		
+		System.out.println("publicStaticInnerClass.staticMethod();");
+		NestedClassTest.PublicStaticInnerClass.staticMethod();
+		//PublicStaticInnerClass.staticMethod();
+		System.out.println("--------------------------------------");
 		
 		System.out.println("publicStaticInnerClass.method();");
 		PublicStaticInnerClass publicStaticInnerClass = new PublicStaticInnerClass();
+		//NestedClassTest.PublicStaticInnerClass publicStaticInnerClass2 = new NestedClassTest.PublicStaticInnerClass();
 		publicStaticInnerClass.method();
 		System.out.println("--------------------------------------");
 		
@@ -24,13 +47,16 @@ public class NestedClassTest {
 		- No enclosing instance of type NestedClassTest is accessible. 
 			Must qualify the allocation with an enclosing instance of type NestedClassTest 
 			(e.g. x.new A()where x is an instance of NestedClassTest).*/
-		//PublicNonStaticInnerClass publicNonStaticInnerClass = new PublicNonStaticInnerClass();
-		//publicNonStaticInnerClass.method();
-		//PublicNonStaticInnerClass publicNonStaticInnerClass = new PublicNonStaticInnerClass();
-		//publicNonStaticInnerClass.method();
+		NestedClassTest nestedClassTest = new NestedClassTest();
+		System.out.println("publicNonStaticInnerClass.method();");
+		NestedClassTest.PublicNonStaticInnerClass publicNonStaticInnerClass = nestedClassTest.new PublicNonStaticInnerClass();
+		publicNonStaticInnerClass.method();
+		System.out.println("--------------------------------------");
 		
-		//PrivateNonStaticInnerClass privateNonStaticInnerClass = new PrivateNonStaticInnerClass();
-		//privateNonStaticInnerClass.method();
+		System.out.println("privateNonStaticInnerClass.method();");
+		NestedClassTest.PrivateNonStaticInnerClass privateNonStaticInnerClass = nestedClassTest.new PrivateNonStaticInnerClass();
+		privateNonStaticInnerClass.method();
+		System.out.println("--------------------------------------");
 	}
 	public void method3() {
 		PublicNonStaticInnerClass publicNonStaticInnerClass = new PublicNonStaticInnerClass();
@@ -41,6 +67,8 @@ public class NestedClassTest {
 	}
 	
 	public void method1() {
+		String localValue = "localValue";
+
 		class LocalClass{
 			//메소드 내부에서만 존재
 			public void method(){
@@ -48,6 +76,10 @@ public class NestedClassTest {
 				System.out.println(privateStaticFiled);
 				System.out.println(publicNonStaticFiled);
 				System.out.println(privateNonStaticFiled);
+				
+				//자바 1.7까지는 localClass가 지역변수에 접근 불가능 하였지만
+				//1.8부터는 접근가능 하도록 변경되었다.
+				System.out.println(localValue);
 			}
 		}
 		System.out.println("localClass.method();");
@@ -89,6 +121,12 @@ public class NestedClassTest {
 			//System.out.println(publicNonStaticFiled);
 			//System.out.println(privateNonStaticFiled);
 		}
+		public static void staticMethod(){
+			System.out.println(publicStaticFiled);
+			System.out.println(privateStaticFiled);
+			//System.out.println(publicNonStaticFiled);
+			//System.out.println(privateNonStaticFiled);
+		}
 	}
 	private static class PrivateStaticInnerClass {
 		public static String a2;
@@ -122,9 +160,7 @@ public class NestedClassTest {
 		public String c4;
 		private String d4;
 		
-		public void method(){
-			method1();
-			method1();
+		public void method(){			
 			System.out.println(publicStaticFiled);
 			System.out.println(privateStaticFiled);
 			System.out.println(publicNonStaticFiled);
